486ecce66e1d87d1de30cea461af2bed69f51ea5
Karen-he <helloKarenH@users.noreply.github.com>
2018-06-14 23:56:55 +1000

    update test

--- account/src/test/java/com/myrest/account/AccountApplicationTests.java
+++ account/src/test/java/com/myrest/account/AccountApplicationTests.java
@@ -2,15 +2,44 @@ package com.myrest.account;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Collections;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
 public class AccountApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AccountRepository accountRepository;
+
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception{
+	    Mockito.when(accountRepository.findAll()).thenReturn(
+                Collections.emptyList()
+        );
+
+	    MvcResult mvcResult = mockMvc.perform(
+	            MockMvcRequestBuilders.get("/mysql/accounts")
+                .accept(MediaType.APPLICATION_JSON)
+        ).andReturn();
+
+	    System.out.println(mvcResult.getResponse());
+
+	    Mockito.verify(accountRepository).findAll();
 	}
 
 }
